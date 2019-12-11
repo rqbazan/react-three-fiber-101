8b106908e6696666cb70e8a4fa414861afe97a65
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2019-12-11 13:26:27 -0500

    refactor: re-order cube pieces

--- src/entities/cube.js
+++ src/entities/cube.js
@@ -20,34 +20,31 @@ const clockwiseNewPositions = getNewPositions(3, 90)
 const anticlockwiseNewPositions = getNewPositions(3, -90)
 
 export default class Cube {
+  static angles = {
+    CLOCKWISE: 90,
+    ANTICLOCKWISE: -90
+  }
+
+  static axisVectors = {
+    X: [1, 0, 0],
+    Y: [0, 1, 0],
+    Z: [0, 0, 1]
+  }
+
   constructor() {
+    // prettier-ignore
     this.pieces = {
-      ULF: new Piece(0),
-      UF: new Piece(1),
-      URF: new Piece(2),
-      FL: new Piece(3),
-      F: new Piece(4),
-      FR: new Piece(5),
-      DLF: new Piece(6),
-      DF: new Piece(7),
-      DRF: new Piece(8),
-      UL: new Piece(9),
-      U: new Piece(10),
-      UR: new Piece(11),
-      L: new Piece(12),
-      R: new Piece(13),
-      DL: new Piece(14),
-      D: new Piece(15),
-      DR: new Piece(16),
-      ULB: new Piece(17),
-      UB: new Piece(18),
-      URB: new Piece(19),
-      BL: new Piece(20),
-      B: new Piece(21),
-      BR: new Piece(22),
-      DLB: new Piece(23),
-      DB: new Piece(24),
-      DRB: new Piece(25)
+      ULF: new Piece(0), UF: new Piece(1), URF: new Piece(2),
+      FL:  new Piece(3), F:  new Piece(4), FR:  new Piece(5),
+      DLF: new Piece(6), DF: new Piece(7), DRF: new Piece(8),
+
+      UL: new Piece(9),  U: new Piece(10), UR: new Piece(11),
+      L:  new Piece(12),   /* (0,0,0) */   R:  new Piece(13),
+      DL: new Piece(14), D: new Piece(15), DR: new Piece(16),
+
+      ULB: new Piece(17), UB: new Piece(18), URB: new Piece(19),
+      BL:  new Piece(20), B:  new Piece(21), BR:  new Piece(22),
+      DLB: new Piece(23), DB: new Piece(24), DRB: new Piece(25)
     }
 
     // prettier-ignore
