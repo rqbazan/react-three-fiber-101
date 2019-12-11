db16df7b82d61de4be89a1c2b39010df22097c0c
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2019-12-10 23:16:45 -0500

    fix: avoid execute action for  special keys along with hot keys

--- src/hooks/use-hot-keys.js
+++ src/hooks/use-hot-keys.js
@@ -3,7 +3,7 @@ import useEventListener from '@use-it/event-listener'
 export default function useHotKeys(actions) {
   useEventListener('keydown', e => {
     const action = actions[e.code]
-    if (action) {
+    if (action && !e.ctrlKey && !e.metaKey) {
       action()
     }
   })
