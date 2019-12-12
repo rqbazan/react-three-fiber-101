ae34265c268f108c9217262acff0cbaef198e984
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2019-12-12 01:41:30 -0500

    feat: add buttons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,24 @@
 import React from 'react'
 import Scene from 'components/scene'
+import Button from 'components/button'
 import './style.css'
 
 export default function IndexPage() {
-  return <Scene />
+  return (
+    <>
+      <Scene />
+      <div className="btns-container">
+        <div className="flex flex-col md:flex-row btns-group">
+          <Button className="bg-green">F</Button>
+          <Button className="bg-white">D</Button>
+          <Button className="bg-red">R</Button>
+        </div>
+        <div className="flex flex-col md:flex-row btns-group">
+          <Button className="bg-blue">B</Button>
+          <Button className="bg-yellow">U</Button>
+          <Button className="bg-orange">L</Button>
+        </div>
+      </div>
+    </>
+  )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        red: '#FF0000',
+        blue: '#3D81F6',
+        green: '#009D54',
+        orange: '#FF6C00',
+        yellow: '#FDCC09',
+        white: '#FFFFFF'
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
