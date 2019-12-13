94d11e1c0ac3021dcf731bc1fe9d1fb574335ae5
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2019-12-13 04:35:31 -0500

    fix: add missing rounded style for modal

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -4,7 +4,7 @@ import Close from 'icons/close.svg'
 export default function Modal({ children, onClose }) {
   return (
     <div className="md:flex md:justify-center md:items-center md:h-screen">
-      <div className="p-8 bg-gray-100 h-screen md:w-full md:max-w-sm md:border md:border-gray-400 md:h-auto md:shadow-lg">
+      <div className="p-8 bg-gray-100 h-screen md:w-full md:max-w-sm md:border md:border-gray-400 md:h-auto md:shadow-lg md:rounded">
         <div className="flex justify-end">
           <Close
             role="button"
