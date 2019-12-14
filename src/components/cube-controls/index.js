31b7a561f9d063aa80b89e1b60a091c93f0587e8
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2019-12-14 11:43:29 -0500

    chore: commit for save code

--- src/components/cube-controls/index.js
+++ src/components/cube-controls/index.js
@@ -1,145 +1,99 @@
 import React from 'react'
+import ClockwiseIcon from 'icons/clockwise.svg'
+import CounterClockwiseIcon from 'icons/counter-clockwise.svg'
 import RoundedButton from '../rounded-button'
 import styles from './styles.module.css'
 
+const initControls = {
+  F: { face: 'F', color: 'green' },
+  D: { face: 'D', color: 'white' },
+  R: { face: 'R', color: 'orange' },
+  B: { face: 'B', color: 'blue' },
+  U: { face: 'U', color: 'yellow' },
+  L: { face: 'L', color: 'red' },
+  M: { face: 'M' },
+  S: { face: 'S' },
+  E: { face: 'E' }
+}
+
+const initSlices = {
+  M: ['F', 'U', 'B', 'D'],
+  S: ['L', 'U', 'B', 'D'],
+  E: ['R', 'F', 'L', 'B']
+}
+
+function getControlClassName(control) {
+  if (control.color) {
+    return `bg-pegatine-${control.color}`
+  }
+
+  return 'bg-black text-white'
+}
+
 export default function CubeControls({ onControlClick }) {
+  const [slices, setSlices] = React.useState(initSlices)
+  const [controls, setControls] = React.useState(initControls)
+
   function onClick(e) {
     const faceName = e.target.dataset.face[0]
     const inversed = e.target.dataset.face.endsWith("'")
+
     onControlClick(faceName, inversed)
+
+    if ('MSE'.includes(faceName)) {
+      let newFaceNames
+      const faceNames = slices[faceName]
+
+      if (inversed) {
+        newFaceNames = [faceNames[3], ...faceNames.slice(1)]
+      } else {
+        newFaceNames = [...faceNames.slice(1), faceNames[0]]
+      }
+
+      for (let i = 0; i < 4; i += 1) {
+        controls[newFaceNames[i]].face = faceNames[i]
+      }
+      slices[faceName] = newFaceNames
+
+      setControls({ ...controls })
+      setSlices({ ...slices })
+    }
   }
 
   return (
     <>
       <div className={`${styles.container} ${styles.left}`}>
-        <RoundedButton
-          data-face="F"
-          className="bg-pegatine-green"
-          onClick={onClick}
-        >
-          F
-        </RoundedButton>
-        <RoundedButton
-          data-face="D"
-          className="bg-pegatine-white"
-          onClick={onClick}
-        >
-          D
-        </RoundedButton>
-        <RoundedButton
-          data-face="R"
-          className="bg-pegatine-orange"
-          onClick={onClick}
-        >
-          R
-        </RoundedButton>
-        <RoundedButton
-          data-face="B"
-          className="bg-pegatine-blue"
-          onClick={onClick}
-        >
-          B
-        </RoundedButton>
-        <RoundedButton
-          data-face="U"
-          className="bg-pegatine-yellow"
-          onClick={onClick}
-        >
-          U
-        </RoundedButton>
-        <RoundedButton
-          data-face="L"
-          className="bg-pegatine-red"
-          onClick={onClick}
-        >
-          L
-        </RoundedButton>
-        <RoundedButton
-          data-face="M"
-          className="bg-black text-white"
-          onClick={onClick}
-        >
-          M
-        </RoundedButton>
-        <RoundedButton
-          data-face="S"
-          className="bg-black text-white"
-          onClick={onClick}
-        >
-          S
-        </RoundedButton>
-        <RoundedButton
-          data-face="E"
-          className="bg-black text-white"
-          onClick={onClick}
-        >
-          E
-        </RoundedButton>
+        {Object.keys(controls).map(key => {
+          const control = controls[key]
+
+          return (
+            <RoundedButton
+              key={`control-for-${key}`}
+              data-face={control.face}
+              className={getControlClassName(control)}
+              onClick={onClick}
+            >
+              {control.color ? <ClockwiseIcon /> : control.face}
+            </RoundedButton>
+          )
+        })}
       </div>
       <div className={`${styles.container} ${styles.right}`}>
-        <RoundedButton
-          data-face="F'"
-          className="bg-pegatine-green"
-          onClick={onClick}
-        >
-          F&apos;
-        </RoundedButton>
-        <RoundedButton
-          data-face="D'"
-          className="bg-pegatine-white"
-          onClick={onClick}
-        >
-          D&apos;
-        </RoundedButton>
-        <RoundedButton
-          data-face="R'"
-          className="bg-pegatine-orange"
-          onClick={onClick}
-        >
-          R&apos;
-        </RoundedButton>
-        <RoundedButton
-          data-face="B'"
-          className="bg-pegatine-blue"
-          onClick={onClick}
-        >
-          B&apos;
-        </RoundedButton>
-        <RoundedButton
-          data-face="U'"
-          className="bg-pegatine-yellow"
-          onClick={onClick}
-        >
-          U&apos;
-        </RoundedButton>
-        <RoundedButton
-          data-face="L'"
-          className="bg-pegatine-red"
-          onClick={onClick}
-        >
-          L&apos;
-        </RoundedButton>
-        <RoundedButton
-          data-face="M'"
-          className="bg-black text-white"
-          onClick={onClick}
-        >
-          M&apos;
-        </RoundedButton>
-        <RoundedButton
-          data-face="S'"
-          className="bg-black text-white"
-          onClick={onClick}
-        >
-          S&apos;
-        </RoundedButton>
-        <RoundedButton
-          data-face="E'"
-          className="bg-black text-white"
-          onClick={onClick}
-        >
-          E&apos;
-        </RoundedButton>
+        {Object.keys(controls).map(key => {
+          const control = controls[key]
+          const faceName = `${control.face}'`
+
+          return (
+            <RoundedButton
+              key={`control-for-${key}-inversed`}
+              data-face={faceName}
+              className={getControlClassName(control)}
+              onClick={onClick}
+            >
+              {control.color ? <CounterClockwiseIcon /> : faceName}
+            </RoundedButton>
+          )
+        })}
       </div>
     </>
   )
