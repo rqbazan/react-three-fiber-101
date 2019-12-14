b725dd183b2c9b7c9fecca243a4190d6a2099630
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2019-12-13 19:14:40 -0500

    fix: mood sad icon color

--- src/components/field-error/index.js
+++ src/components/field-error/index.js
@@ -5,8 +5,8 @@ import styles from './styles.module.css'
 export default function FieldError({ children }) {
   return (
     <div className="flex rounded bg-red-300 px-3 py-3 items-center text-sm text-red-800">
-      <i className="h-6 w-6 mr-1">
-        <MoodSad className={styles.icon} />
+      <i className={styles.icon}>
+        <MoodSad />
       </i>
       {children}
     </div>
